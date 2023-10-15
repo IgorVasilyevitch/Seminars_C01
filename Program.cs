c72b4fd320578e2f97c49936e988e15246ef5aa8
igorvasilyevitch <igorvasilyevitch@yandex.ru>
2023-10-15 15:44:39 +0300

    Вторая задача

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿System.Console.Write("Введите число ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number >99)
+{
+while (number>999)
+{
+    number /=10;
+}
+System.Console.WriteLine(number%10);
+
+}
+else
+{
+System.Console.WriteLine("ОШИБКА! Третьей цифры нет");    
+}
